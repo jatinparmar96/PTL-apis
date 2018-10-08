b88d146c1dcb512e068770b99c8059d0d26fd73d
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2018-10-08 15:26:25 +0530

    Created Finished Product
    Created Model, Controller, Migration

---
+++ database/migrations/2018_10_08_152527_create_finished_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFinishedProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('finished_products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('finished_products');
+    }
+}
