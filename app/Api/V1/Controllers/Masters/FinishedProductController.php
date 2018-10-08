1d580f2a7707db68ecd783a10acfa022ca15c602
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2018-10-08 15:30:30 +0530

    Wrote controller for Finished Product
    Wrote the code for add/edit view finished products

--- app/Api/V1/Controllers/Masters/FinishedProductController.php
+++ app/Api/V1/Controllers/Masters/FinishedProductController.php
@@ -4,11 +4,11 @@ namespace App\Api\V1\Controllers\Masters;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Model\RawProduct;
 use Illuminate\Support\Facades\Input;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use App\Api\V1\Controllers\Authentication\TokenController;
 use Illuminate\Support\Facades\DB;
+use App\Model\FinishedProduct;
 
 
 class FinishedProductController extends Controller
@@ -20,7 +20,7 @@ class FinishedProductController extends Controller
         $current_company_id = TokenController::getCompanyId();
         if($id == 'new')
         {
-            $count = RawProduct::where('product_name',$request->get('raw_product_name'))
+            $count = FinishedProduct::where('product_name',$request->get('finished_product_name'))
                                 ->where('company_id',$current_company_id)
                                 ->count();
             if($count>0)
@@ -31,7 +31,7 @@ class FinishedProductController extends Controller
             }
             else
             {
-                $raw = new RawProduct();
+                $raw = new FinishedProduct();
                 $raw->company_id = $current_company_id;
                 $message = "Record added Successfully";
             }
@@ -40,26 +40,26 @@ class FinishedProductController extends Controller
         else
         {
             $message = 'Record Updated Successfully';
-            $raw = RawProduct::findOrFail($id);
+            $raw = FinishedProduct::findOrFail($id);
         }
         if($status)
         {
-            $raw->product_name = Input::get('raw_product_name');
-            $raw->product_display_name = Input::get('raw_product_display_name');
-            $raw->product_code = Input::get('raw_product_code');
-            $raw->product_uom = Input::get('raw_product_uom');
-            $raw->product_conv_uom = Input::get('raw_product_conv_uom');
-            $raw->conv_factor = Input::get('raw_product_conv_factor');
-            $raw->batch_type = Input::get('raw_product_batch_type');
-            $raw->stock_ledger = Input::get('raw_product_maintain_stock_ledger');
-            $raw->product_rate_pick = Input::get('raw_product_rate_pick_from');
-            $raw->product_purchase_rate = Input::get('raw_product_purchase_rate');
-            $raw->mrp_rate = Input::get('raw_product_mrp_rate');
-            $raw->sales_rate = Input::get('raw_product_sales_rate');
-            $raw->gst_rate = Input::get('raw_product_gst_slot');
-            $raw->max_level = Input::get('raw_product_max_level');
-            $raw->min_level = Input::get('raw_product_min_level');
-            $raw->description = Input::get('raw_product_description');
+            $raw->product_name = Input::get('finished_product_name');
+            $raw->product_display_name = Input::get('finished_product_display_name');
+            $raw->product_code = Input::get('finished_product_code');
+            $raw->product_uom = Input::get('finished_product_uom');
+            $raw->product_conv_uom = Input::get('finished_product_conv_uom');
+            $raw->conv_factor = Input::get('finished_product_conv_factor');
+            $raw->batch_type = Input::get('finished_product_batch_type');
+            $raw->stock_ledger = Input::get('finished_product_maintain_stock_ledger');
+            $raw->product_rate_pick = Input::get('finished_product_rate_pick_from');
+            $raw->product_purchase_rate = Input::get('finished_product_purchase_rate');
+            $raw->mrp_rate = Input::get('finished_product_mrp_rate');
+            $raw->sales_rate = Input::get('finished_product_sales_rate');
+            $raw->gst_rate = Input::get('finished_product_gst_slot');
+            $raw->max_level = Input::get('finished_product_max_level');
+            $raw->min_level = Input::get('finished_product_min_level');
+            $raw->description = Input::get('finished_product_description');
             try
             {
                 $raw->save();
@@ -69,7 +69,7 @@ class FinishedProductController extends Controller
                 $status = false;
                 $message = 'Something is wrong. Kindly Contact Admin';
             }
-            $raw = $this->query()->where('rp.id',$raw->id)->first();
+            $raw = $this->query()->where('fp.id',$raw->id)->first();
             return response()->json([
                 'status' => $status,
                 'data' => $raw,
@@ -92,36 +92,36 @@ class FinishedProductController extends Controller
     {
 
         $current_company_id = TokenController::getCompanyId();
-        $query = DB::table('raw_products as rp')
-                ->leftJoin('unit_of_measurements as uom1','rp.product_uom','uom1.id')
-                ->leftJoin('unit_of_measurements as uom2','rp.product_conv_uom','uom2.id')
+        $query = DB::table('finished_products as fp')
+                ->leftJoin('unit_of_measurements as uom1','fp.product_uom','uom1.id')
+                ->leftJoin('unit_of_measurements as uom2','fp.product_conv_uom','uom2.id')
                 ->select(
-                'rp.id','rp.product_name','rp.product_display_name','rp.product_code','rp.conv_factor','rp.batch_type','rp.stock_ledger','rp.product_rate_pick','rp.product_purchase_rate','rp.mrp_rate','rp.sales_rate','rp.gst_rate','rp.max_level','rp.min_level','rp.description'
+                'fp.id','fp.product_name','fp.product_display_name','fp.product_code','fp.conv_factor','fp.batch_type','fp.stock_ledger','fp.product_rate_pick','fp.product_purchase_rate','fp.mrp_rate','fp.sales_rate','fp.gst_rate','fp.max_level','fp.min_level','fp.description'
                 )
                 ->addSelect('uom1.unit_name')
                 ->addSelect('uom2.unit_name as conversion_uom')
-                ->where('rp.company_id',$current_company_id);
+                ->where('fp.company_id',$current_company_id);
         return $query;
     }
 
     public function TableColumn()
     {         
         $TableColumn = array(
-                       "id"=>"rp.id",
-                       "product_name"=>"rp.product_name",                       
-                       "product_display_name"=>"rp.product_display_name",
-                       "product_code"=>"rp.product_code",                       
-                       "conv_factor"=>"rp.conv_factor",
-                       "batch_type"=>"rp.batch_type",
-                       "stock_ledger"=>"rp.stock_ledger",
-                       "product_rate_pick"=>"rp.product_rate_pick",
-                       "product_purchase_rate"=>"rp.product_purchase_rate",
-                       "mrp_rate"=>"rp.mrp_rate",
-                       "sales_rate"=>"rp.sales_rate",
-                       "gst_rate"=>"rp.gst_rate",
-                       "max_level"=>"rp.max_level",
-                       "min_level"=>"rp.min_level",
-                       "description"=>"rp.description",
+                       "id"=>"fp.id",
+                       "product_name"=>"fp.product_name",                       
+                       "product_display_name"=>"fp.product_display_name",
+                       "product_code"=>"fp.product_code",                       
+                       "conv_factor"=>"fp.conv_factor",
+                       "batch_type"=>"fp.batch_type",
+                       "stock_ledger"=>"fp.stock_ledger",
+                       "product_rate_pick"=>"fp.product_rate_pick",
+                       "product_purchase_rate"=>"fp.product_purchase_rate",
+                       "mrp_rate"=>"fp.mrp_rate",
+                       "sales_rate"=>"fp.sales_rate",
+                       "gst_rate"=>"fp.gst_rate",
+                       "max_level"=>"fp.max_level",
+                       "min_level"=>"fp.min_level",
+                       "description"=>"fp.description",
                        );
         return $TableColumn;
     }
@@ -167,7 +167,7 @@ class FinishedProductController extends Controller
         return response()->json([
                 'status' => true,
                 'status_code' => 200,
-                'message' => 'Raw Product List',
+                'message' => 'Finished Product List',
                 'data' => $result
                 ]);
     }
@@ -181,7 +181,7 @@ class FinishedProductController extends Controller
         return response()->json([
                 'status' => true,
                 'status_code' => 200,
-                'message' => 'Raw Product Full List',
+                'message' => 'Finished Product Full List',
                 'data' => $result
                 ]);
     }
